0d12665843148a77099a95724dfe009ebe034805
daxp472 <daxpatel.cg@gmail.com>
2025-03-01 11:59:01 +0530

    db.js fixed with the better error handling

--- Backend/Auth/src/config/db.js
+++ Backend/Auth/src/config/db.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+  if (!process.env.MONGODB_URI) {
+    throw new Error('MONGODB_URI not configured in environment variables');
+  }
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected successfully');
